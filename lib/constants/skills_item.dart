4e9aee8d3fe71e95af7ab403cca8b6c16dda9edf
VasistaTech <vasistatech19@gmail.com>
2024-11-15 19:06:55 +0530

    Update or create file via GitHub API

--- lib/constants/skills_item.dart
+++ lib/constants/skills_item.dart
@@ -1,73 +1,17 @@
-const List<Map> platformItems = [
-  {
-    "img": "assets/big data.png",
-    "title": "Big Data",
-  },
-  {
-    "img": "assets/data engineering.png",
-    "title": "Data Engineering",
-  },
-  {
-    "img": "assets/Data_science.png",
-    "title": "Data Science",
-  },
-  {
-    "img": "assets/Machine_learning.png",
-    "title": "Machine Learning",
-  },
-  {
-    "img": "assets/flutter.png",
-    "title": "Flutter Development",
-  },
+const List<Map> platformItems = [ 
+ { 
+
+"img" :"assets/newplot.png",
+
+"title": "A",
+ },
 ];
 
-const List<Map> skillItem = [
-  {
-    "img": "assets/python.png",
-    "title": "Python"
-  },
-  {
-    "img": "assets/big data.png",
-    "title": "Big Data"
-  },
-  {
-    "img": "assets/spark.png",
-    "title": "Spark"
-  },
-  {
-    "img": "assets/pyspark.png",
-    "title": "PySpark"
-  },
-  {
-    "img": "assets/AzureCloud.webp",
-    "title": "Azure Cloud"
-  },
-  {
-    "img": "assets/DataBricks.png",
-    "title": "Data Bricks"
-  },
-  {
-    "img": "assets/odi.jpg",
-    "title": "ODI"
-  },
-  {
-    "img": "assets/oracle-database.webp",
-    "title": "SQL/PLSQL"
-  },
-  {
-    "img": "assets/Hive.png",
-    "title": "Hive(HQL)"
-  },
-  {
-    "img": "assets/Unix.png",
-    "title": "Shell Scripting"
-  },
-  {
-    "img": "assets/flutter.png",
-    "title": "Flutter"
-  },
-  {
-    "img": "assets/dart.png",
-    "title": "Dart"
-  },
-];
+ const List<Map> skillItem = [ 
+ { 
+
+"img" :"assets/IMG_6003.JPG",
+
+"title": "JP",
+ },
+]; 
