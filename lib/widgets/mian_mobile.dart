c2433e6887c93b24dde4bce729bf6985b7d80d62
Sumanth Manjunath <30977397+sumanthvasista107@users.noreply.github.com>
2024-11-15 20:06:23 +0530

    Update mian_mobile.dart

--- lib/widgets/mian_mobile.dart
+++ lib/widgets/mian_mobile.dart
@@ -20,7 +20,7 @@
 
         void downloadPDF() {
 
-        const url = 'https://testRepo_test.vercel.app/assets/public/Test.pdf'; // The relative path to your PDF in the web directory
+        const url = 'https://testrepo_test.vercel.app/assets/public/Test.pdf'; // The relative path to your PDF in the web directory
 
         final anchor = html.AnchorElement(href: url)
 
