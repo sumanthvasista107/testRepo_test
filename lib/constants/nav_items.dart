578e0ff00fcb4c2f2d0661fd56f767cd5371624f
VasistaTech <vasistatech19@gmail.com>
2024-11-15 19:06:54 +0530

    Update or create file via GitHub API

--- lib/constants/nav_items.dart
+++ lib/constants/nav_items.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
-List<String> navtitles=[
-  "Home",
-  "Skills",
-  "Projects",
-  "Contact",
-  "Blog",
-];
-// For mobiles
-List<IconData> navIcons =[
-  Icons.home,
-  Icons.handyman_outlined,
-  Icons.apps,
-  Icons.quick_contacts_mail,
-  Icons.web,
-];
+                    List < String > navtitles = ["Home","Skills","Projects","Contact","Blog"];
+
+                    // For mobiles
+
+                    List < IconData > navIcons = [
+
+                        Icons.home,
+
+                        Icons.handyman_outlined,
+
+                        Icons.apps,
+
+                        Icons.quick_contacts_mail,
+
+                        Icons.web,
+
+                    ];
