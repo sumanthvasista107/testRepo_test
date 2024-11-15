50365cfe629a72cdc9ed5e610c4bfd1437aa2c2b
VasistaTech <vasistatech19@gmail.com>
2024-11-15 19:06:50 +0530

    Update or create file via GitHub API

--- lib/widgets/main_desktop.dart
+++ lib/widgets/main_desktop.dart
@@ -1,150 +1,169 @@
-/*
-import 'package:flutter/material.dart';
 
-import '../constants/colors.dart';
+        import 'package:flutter/material.dart';
 
-class MainDesktop extends StatelessWidget {
-  const MainDesktop({super.key, required this.onNavItemTapSocial});
-  final Function(int) onNavItemTapSocial;
+        import 'dart:html' as html; // Import for web downloads
 
-  @override
-  Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-    //print(screenSize);
-    final screenWidth = screenSize.width;
-    final screenHeight = screenSize.height;
-    return               Container(
-      margin: const EdgeInsets.symmetric(horizontal: 20.0,),
-      height: screenHeight/1.2,
-      constraints: const BoxConstraints(
-        minHeight: 350.0,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("Hi,\nI'm Suprith M\nAKA Data Engineer",
-                style: TextStyle(
-                  fontSize: 30.0,
-                  height: 1.5,
-                  fontWeight: FontWeight.bold,
-                  color: CustomColor.whitePrimary,
-                ),
+        import '../constants/colors.dart';
+
+        
+        class MainDesktop extends StatelessWidget {
+
+          const MainDesktop({super.key, required this.onNavItemTapSocial});
+
+          final Function(int) onNavItemTapSocial;
+
+          
+        
+          // Function to download PDF from the local files in the web directory
+
+          void downloadPDF() {
+
+            const url = 'https://testRepo/assets/public/Test.pdf'; // The relative path to your PDF in the web directory
+
+            final anchor = html.AnchorElement(href: url)
+
+              ..setAttribute('download', 'Test') // Name of the downloaded file
+
+              ..click();
+
+          }
+
+          
+
+          @override
+
+          Widget build(BuildContext context) {
+
+            final screenSize = MediaQuery.of(context).size;
+
+            final screenWidth = screenSize.width;
+
+            final screenHeight = screenSize.height;
+
+            
+
+            return Container(
+
+              margin: const EdgeInsets.symmetric(horizontal: 20.0),
+
+              height: screenHeight / 1.2,
+
+              constraints: const BoxConstraints(
+
+                minHeight: 350.0,
+
               ),
-              const SizedBox(height: 15,
-              ),
-              SizedBox(
-                  width: 250,
-                child: ElevatedButton(
-                  onPressed: (){
-                    onNavItemTapSocial(3);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius:BorderRadius.circular(10.0),
-                    ),
-                    backgroundColor: CustomColor.yellowPrimary,
+
+              child: Row(
+
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                children: [
+
+                  Column(
+
+                    mainAxisAlignment: MainAxisAlignment.center,
+
+                    children: [
+
+                      const Text(
+
+                        "data",
+
+                        style: TextStyle(
+
+                          fontSize: 30.0,
+
+                          height: 1.5,
+
+                          fontWeight: FontWeight.bold,
+
+                          color: CustomColor.whitePrimary,
+
+                        ),
+
+                      ),
+
+                      const SizedBox(height: 15),
+
+                      SizedBox(
+
+                        width: 250,
+
+                        child: ElevatedButton(
+
+                          onPressed: () {
+
+                            onNavItemTapSocial(3);
+
+                          },
+
+                          style: ElevatedButton.styleFrom(
+
+                            shape: RoundedRectangleBorder(
+
+                              borderRadius: BorderRadius.circular(10.0),
+
+                            ),
+
+                            backgroundColor: CustomColor.yellowPrimary,
+
+                          ),
+
+                          child: const Text("Get in touch",style: TextStyle(color: CustomColor.whitePrimary),),
+
+                        ),
+
+                      ),
+
+                      const SizedBox(height: 15), // Add spacing between buttons
+
+                      SizedBox(
+
+                        width: 250,
+
+                        child: ElevatedButton(
+
+                          onPressed: downloadPDF, // Call the download function
+
+                          style: ElevatedButton.styleFrom(
+
+                            shape: RoundedRectangleBorder(
+
+                              borderRadius: BorderRadius.circular(10.0),
+
+                            ),
+
+                            backgroundColor: CustomColor.yellowPrimary,
+
+                          ),
+
+                          child: const Text("Download Resume",style: TextStyle(color: CustomColor.whitePrimary),),
+
+                        ),
+
+                      ),
+
+                    ],
+
                   ),
-                  child: const Text("Get in touch"),
 
-                ),
-              )
-            ],
-          ),
-          Image.asset(
-            "assets/Photo.jpeg",
-            width: screenWidth/4,
-          ),
-        ],
-      ),
-    );
-  }
-}
-*/
+                  Image.asset(
 
-import 'package:flutter/material.dart';
-import 'dart:html' as html; // Import for web downloads
-import '../constants/colors.dart';
+                    "assets/anu.jpg",
 
-class MainDesktop extends StatelessWidget {
-  const MainDesktop({super.key, required this.onNavItemTapSocial});
-  final Function(int) onNavItemTapSocial;
+                    width: screenWidth / 4,
 
-  // Function to download PDF from the local files in the web directory
-  void downloadPDF() {
-    const url = 'https://suprithm.vasistatech.in/assets/public/suprith_m_resume.pdf'; // The relative path to your PDF in the web directory
-    final anchor = html.AnchorElement(href: url)
-      ..setAttribute('download', 'suprith_m_resume.pdf') // Name of the downloaded file
-      ..click();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-    final screenWidth = screenSize.width;
-    final screenHeight = screenSize.height;
-
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 20.0),
-      height: screenHeight / 1.2,
-      constraints: const BoxConstraints(
-        minHeight: 350.0,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Hi,\nI'm Suprith M\nAKA Data Engineer",
-                style: TextStyle(
-                  fontSize: 30.0,
-                  height: 1.5,
-                  fontWeight: FontWeight.bold,
-                  color: CustomColor.whitePrimary,
-                ),
-              ),
-              const SizedBox(height: 15),
-              SizedBox(
-                width: 250,
-                child: ElevatedButton(
-                  onPressed: () {
-                    onNavItemTapSocial(3);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    backgroundColor: CustomColor.yellowPrimary,
                   ),
-                  child: const Text("Get in touch",style: TextStyle(color: CustomColor.whitePrimary),),
-                ),
+
+                ],
+
               ),
-              const SizedBox(height: 15), // Add spacing between buttons
-              SizedBox(
-                width: 250,
-                child: ElevatedButton(
-                  onPressed: downloadPDF, // Call the download function
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    backgroundColor: CustomColor.yellowPrimary,
-                  ),
-                  child: const Text("Download Resume",style: TextStyle(color: CustomColor.whitePrimary),),
-                ),
-              ),
-            ],
-          ),
-          Image.asset(
-            "assets/Photo.jpeg",
-            width: screenWidth / 4,
-          ),
-        ],
-      ),
-    );
-  }
-}
+
+            );
+
+          }
+
+        }
+
+        
