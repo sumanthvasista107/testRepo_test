4de0185aaa9f2e919ac81b58b119558d584c91dc
VasistaTech <vasistatech19@gmail.com>
2024-11-15 19:06:58 +0530

    Update or create file via GitHub API

--- lib/constants/sns_links.dart
+++ lib/constants/sns_links.dart
@@ -1,7 +1,15 @@
-class SnsLinks{
-  static const String github="https://github.com/suprithvasista/";
-  static const String linkedin="https://www.linkedin.com/in/suprithm1/";
-  static const String devpost="https://devpost.com/suprithvasista829";
-  static const String gmail="suprithvasista829@gmail.com";
-  static const String medium="https://medium.com/@suprithvasista829";
-}
+
+        class SnsLinks{
+
+          static const String github="https://github.com/";
+
+          static const String linkedin="https://www.linkedin.com/in/";
+
+          static const String devpost="https://devpost.com/";
+
+          static const String gmail="";
+
+          static const String medium="https://medium.com/";
+
+        }
+        
