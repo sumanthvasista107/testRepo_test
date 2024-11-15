34b25d435cfc192c7054fc0a83af3d5f36d494fa
VasistaTech <vasistatech19@gmail.com>
2024-11-15 19:06:51 +0530

    Update or create file via GitHub API

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:porfoliojob/pages/home_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+        import'package:porfoliojob/pages/home_page.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+        void main()
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      title: 'Suprith M',
-      home: const HomePage(),
-    );
-  }
-}
+        {
 
+            runApp(const
+
+        MyApp());
+
+        }
+
+
+        class MyApp extends StatelessWidget {
+
+        const MyApp({super.key});
+
+        
+
+        // This widget is the root of your application.
+
+        @ override
+
+        Widget build(BuildContext context) {
+
+        
+
+        return MaterialApp(
+
+            debugShowCheckedModeBanner: false,
+
+                                        theme: ThemeData.dark(),
+
+        title: 'Test_1',
+
+        home: const
+
+        HomePage(),
+
+        );
+
+        }
+
+        }
